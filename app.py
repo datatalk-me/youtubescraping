7501b28797fb615fe6c0789796056e384eafa4e4
datatalk-me <sandeeprayudu25@gmail.com>
2022-09-09 23:09:38 +0530

    first commit

--- app.py
+++ app.py
@@ -38,10 +38,17 @@ def videos():
     if request.method == 'POST':
         youtuber = request.form['search']
 
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+
 
         #driver = webdriver.Chrome(executable_path=r'/home/sandeep/Desktop/youtubeTask/chromedriver')
 
-        driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'))
+        #driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'))
         logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
 
